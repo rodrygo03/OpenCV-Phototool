ca87f50642b330047fa55c6e7f9dbfb8f4332f2d
RodrigoOrozco <rodrigoorozco025@gmail.com>
2024-07-28 15:24:45 -0500

    equalize, lomography, cartoon effects

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 
+#include <iostream>
+
 
 cv::Mat img;
 const char* keys = 
@@ -60,12 +62,101 @@ void showHistoCallback(int state, void* userData)
     cv::imshow("Histogram", histImg);
 };
 
-void equalizeCallback(int state, void* userData){};
-void lomoCallback(int state, void* userData){};
-void cartoonCallback(int state, void* userData){};
+void equalizeCallback(int state, void* userData)
+{
+    cv::Mat result;
+    cv::Mat ycrcb;
+    cv::cvtColor(img, ycrcb, cv::COLOR_BGR2YCrCb);
+
+    // split YCbCr image into diff channels matrix
+    std::vector<cv::Mat> channels;
+    cv::split(ycrcb, channels);
+    cv::equalizeHist(channels[0], channels[0]); // equalize in y only
+
+    cv::merge(channels, ycrcb);
+    cv::cvtColor(ycrcb, result, cv::COLOR_YCrCb2BGR);
+
+    cv::imshow("Equalized", result); 
+};
+
+void lomoCallback(int state, void* userData)
+{
+    cv::Mat result;
+
+    // Look up table:
+    const double exp = std::exp(1.0);
+    cv::Mat lut(1, 256, CV_8UC1);
+    uchar* plut = lut.data;
+    for (int i=0; i<256; i++)
+    {
+        double x = (double) i / 256.0;
+        plut[i] = cvRound(256.0 * (1.0 / (1.0 + std::pow(exp, -((x - 0.5)/1)))));
+    }
+
+    // apply curve transfer only to red channel
+    std::vector<cv::Mat> bgr;
+    cv::split(img, bgr);
+    cv::LUT(bgr[2], lut, bgr[2]);
+    cv::merge(bgr, result);
+
+    // halo with blur to smooth
+    cv::Mat halo(img.rows, img.cols, CV_32FC3, cv::Scalar(0.3, 0.3, 0.3));
+    cv::circle(halo, cv::Point(img.cols/2, img.rows/2), img.cols/3, cv::Scalar(1, 1, 1), -1);
+    cv::blur(halo, halo, cv::Size(img.cols/3 ,img.cols/3));
+
+    // for matrix multiplication
+    cv::Mat resultf;
+    result.convertTo(resultf, CV_32FC3);
+    cv::multiply(resultf, halo, resultf);
+    resultf.convertTo(result, CV_8UC3);
+
+    cv::imshow("Lomograpy", result);
+};
+
+void cartoonCallback(int state, void* userData)
+{
+    // remove some noise
+    cv::Mat imgMedian;
+    cv::medianBlur(img, imgMedian, 7); // Gaussian blur
+    // edge detection
+    cv::Mat imgCanny;
+    cv::Canny(imgMedian, imgCanny, 50, 150);
+    // dilate edges
+    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2, 2));
+    cv::dilate(imgCanny, imgCanny, kernel);
+
+    // rescaleing for pixel multiplication
+    imgCanny = imgCanny/255;
+    imgCanny = 1 - imgCanny;
+
+    cv::Mat imgCannyf;
+    imgCanny.convertTo(imgCannyf, CV_32FC3);
+    blur(imgCannyf, imgCannyf, cv::Size(5, 5));
+
+    // Homogonize color
+    cv::Mat imgBF;
+    cv::bilateralFilter(img, imgBF, 9, 150.0, 150.0);
+
+    cv::Mat result = imgBF/25;
+    result = result * 25; // truncates possible color values
+
+    // merge color and edge results
+    cv::Mat imgCanny3c;
+    cv::Mat cannyChannels[] = {imgCannyf, imgCannyf, imgCannyf};
+    cv::merge(cannyChannels, 3, imgCanny3c);
+
+    cv::Mat resultf;
+    result.convertTo(resultf, CV_32FC3);
+    cv::multiply(resultf, imgCanny3c, resultf);
+    resultf.convertTo(result, CV_8UC3);
+
+    cv::imshow("Result", result);
+
+};
 
 int main(int argc, const char** argv)
 {
+    std::cout << cv::getBuildInformation();
     cv::CommandLineParser parser(argc, argv, keys);
     parser.about("Phototool v1");
     if (parser.has("help")) 
@@ -89,9 +180,9 @@ int main(int argc, const char** argv)
 
     // ui buttons
     cv::createButton("Show histogram", showHistoCallback, NULL, cv::QT_PUSH_BUTTON, 0);
-    // cv::createButton("Equalize histogram", equalizeCallback, NULL, cv::QT_PUSH_BUTTON, 0);
-    // cv::createButton("Lomography effect", lomoCallback, NULL, cv::QT_PUSH_BUTTON, 0);
-    // cv::createButton("Cartoonize effect", cartoonCallback, NULL, cv::QT_PUSH_BUTTON, 0);
+    cv::createButton("Equalize histogram", equalizeCallback, NULL, cv::QT_PUSH_BUTTON, 0);
+    cv::createButton("Lomography effect", lomoCallback, NULL, cv::QT_PUSH_BUTTON, 0);
+    cv::createButton("Cartoonize effect", cartoonCallback, NULL, cv::QT_PUSH_BUTTON, 0);
 
     // Show image
     cv::imshow("Input", img);
